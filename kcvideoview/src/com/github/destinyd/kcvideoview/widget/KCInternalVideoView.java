cb5c3870ac53bd5bd9d27b536b6e8cd2c66684e7
destinyd <destinyd_war@163.com>
2014-04-28 10:37:13 +0800

    video size

--- kcvideoview/src/com/github/destinyd/kcvideoview/widget/KCInternalVideoView.java
+++ kcvideoview/src/com/github/destinyd/kcvideoview/widget/KCInternalVideoView.java
@@ -16,7 +16,7 @@ import java.util.List;
 /**
  * Created by dd on 14-4-16.
  */
-public class KCInternalVideoView extends VideoView implements MediaPlayer.OnPreparedListener {
+public class KCInternalVideoView extends VideoView implements MediaPlayer.OnPreparedListener, MediaPlayer.OnVideoSizeChangedListener {
     List<PlayListObj> mListPlay = null;
     int mHours, mMinutes, mSeconds, mSecondsCount;
     int mSize;
@@ -24,6 +24,8 @@ public class KCInternalVideoView extends VideoView implements MediaPlayer.OnPrep
     int mPlayCount;
     boolean isChangePart = false, isPlaying = false;
     int mChangeSeekTo;
+    MediaPlayer mediaPlayer = null;
+    int mVideoWidth, mVideoHeight;
 
     public KCInternalVideoView(Context context) {
         super(context);
@@ -51,13 +53,13 @@ public class KCInternalVideoView extends VideoView implements MediaPlayer.OnPrep
                     PlayListObj tmp_prev = mListPlay.get(i - 1);
                     if (msec < tmp.getSecondsCount() * 1000 && msec >= tmp_prev.getSecondsCount() * 1000) {
                         setPosition(i + 1, msec - tmp_prev.getSecondsCount() * 1000); // this, 逻辑有问题 还是他本身有问题
-                        if(isPlaying)
+                        if (isPlaying)
                             start();
                         return;
                     }
                 }
                 setPosition(1, msec);
-                if(isPlaying)
+                if (isPlaying)
                     start();
                 return;
             }
@@ -143,6 +145,10 @@ public class KCInternalVideoView extends VideoView implements MediaPlayer.OnPrep
 
     @Override
     public void onPrepared(MediaPlayer mediaPlayer) {
+        if (this.mediaPlayer != mediaPlayer) {
+            this.mediaPlayer = mediaPlayer;
+            this.mediaPlayer.setOnVideoSizeChangedListener(this);
+        }
         Log.d("KCInternalVideoView", "prepared!!!!!!!!!!");
         if (isChangePart) {
             mediaPlayer.seekTo(mChangeSeekTo);
@@ -164,5 +170,81 @@ public class KCInternalVideoView extends VideoView implements MediaPlayer.OnPrep
             return mListPlay.get(part - 2);
         }
         return null;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = getDefaultSize(mVideoWidth, widthMeasureSpec);
+        int height = getDefaultSize(mVideoHeight, heightMeasureSpec);
+        if (mVideoWidth > 0 && mVideoHeight > 0) {
+
+            int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+            int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+            int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+            int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+
+            if (widthSpecMode == MeasureSpec.EXACTLY && heightSpecMode == MeasureSpec.EXACTLY) {
+                // the size is fixed
+                width = widthSpecSize;
+                height = heightSpecSize;
+
+                // for compatibility, we adjust size based on aspect ratio
+                if (mVideoWidth * height < width * mVideoHeight) {
+                    //Log.i("@@@", "image too wide, correcting");
+                    width = height * mVideoWidth / mVideoHeight;
+                } else if (mVideoWidth * height > width * mVideoHeight) {
+                    //Log.i("@@@", "image too tall, correcting");
+                    height = width * mVideoHeight / mVideoWidth;
+                }
+            } else if (widthSpecMode == MeasureSpec.EXACTLY) {
+                // only the width is fixed, adjust the height to match aspect ratio if possible
+                width = widthSpecSize;
+                height = width * mVideoHeight / mVideoWidth;
+                if (heightSpecMode == MeasureSpec.AT_MOST && height > heightSpecSize) {
+                    // couldn't match aspect ratio within the constraints
+                    height = heightSpecSize;
+                }
+            } else if (heightSpecMode == MeasureSpec.EXACTLY) {
+                // only the height is fixed, adjust the width to match aspect ratio if possible
+                height = heightSpecSize;
+                width = height * mVideoWidth / mVideoHeight;
+                if (widthSpecMode == MeasureSpec.AT_MOST && width > widthSpecSize) {
+                    // couldn't match aspect ratio within the constraints
+                    width = widthSpecSize;
+                }
+            } else {
+                // neither the width nor the height are fixed, try to use actual video size
+                width = mVideoWidth;
+                height = mVideoHeight;
+                if (
+//                        heightSpecMode == MeasureSpec.AT_MOST &&
+                        height > heightSpecSize) {
+                    // too tall, decrease both width and height
+                    height = heightSpecSize;
+                    width = height * mVideoWidth / mVideoHeight;
+                }
+                if (
+//                        widthSpecMode == MeasureSpec.AT_MOST &&
+                        width > widthSpecSize) {
+                    // too wide, decrease both width and height
+                    width = widthSpecSize;
+                    height = width * mVideoHeight / mVideoWidth;
+                }
+            }
+            getHolder().setFixedSize(width, height);
+        } else {
+            // no size yet, just adopt the given spec sizes
+        }
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+        mVideoWidth = mp.getVideoWidth();
+        mVideoHeight = mp.getVideoHeight();
+        if (mVideoWidth != 0 && mVideoHeight != 0) {
+            getHolder().setFixedSize(mVideoWidth, mVideoHeight);
+            requestLayout();
+        }
     }
 }
